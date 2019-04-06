32b83208caf367e66ed36fdd8ea56f7eb1374061
Isabelle P. Rosa <ibell.pereira@gmail.com>
2019-04-06 02:51:27 -0300

    alteracoes em utils.c

--- src/utils.c
+++ src/utils.c
@@ -1,60 +1,59 @@
 #include "main.h"
 
-TipoLista* criaListaPopulada(int qtd){
+TipoLista* criaListaPopulada(int qtd) {
 	TipoLista *lista = (TipoLista*) malloc(sizeof(TipoLista));
 	int i;
 
-	//srand(time(NULL));
-
 	criaListaVazia(lista);
 
-	for(i = 0; i<qtd; i++){
-		TipoItem item;
-		item.chave = rand() % 100;
-		insereLista(lista, item);
+	for (i = 0; i < qtd; i++) {
+		insereLista(lista, criaItem());
 	}
 
 	return lista;
 }
 
-TipoLista* clonaLista(TipoLista* lista){
+TipoLista* clonaLista(TipoLista* lista) {
 	TipoLista* clone = (TipoLista*) malloc(sizeof(TipoLista));
 	criaListaVazia(clone);
 
 	TipoCelula* celula;
-	for(celula = lista->primeiro->proximo; celula != NULL; celula = celula->proximo){
+	for (celula = lista->primeiro->proximo; celula != NULL;
+			celula = celula->proximo) {
 		insereLista(clone, celula->item);
 	}
 
 	return clone;
 }
 
-TipoPilha* criaPilhaPopulada(int qtd){
+TipoPilha* criaPilhaPopulada(int qtd) {
 	TipoPilha* pilha = (TipoPilha*) malloc(sizeof(TipoPilha));
 
 	criaPilhaVazia(pilha);
 
 	int i;
-	for(i = 0; i<qtd; i++){
-			TipoItem item;
-			item.chave = rand() % 100;
-			inserePilha(pilha, item);
-		}
+	for (i = 0; i < qtd; i++) {
+		inserePilha(pilha, criaItem());
+	}
 
 	return pilha;
 }
 
-TipoFila* criaFilaPopulada(int qtd){
+TipoFila* criaFilaPopulada(int qtd) {
 	TipoFila* fila = (TipoFila*) malloc(sizeof(TipoFila));
 
 	criaFilaVazia(fila);
 
 	int i;
-	for(i = 0; i<qtd; i++){
-			TipoItem item;
-			item.chave = rand() % 100;
-			insereFila(fila, item);
-		}
+	for (i = 0; i < qtd; i++) {
+		insereFila(fila, criaItem());
+	}
 
 	return fila;
+}
+
+TipoItem criaItem() {
+	TipoItem item;
+	item.chave = rand() % 100;
+	return item;
 }
