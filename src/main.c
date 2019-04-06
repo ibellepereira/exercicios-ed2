17e783231984c0ab042afe948f22b68b937e158d
Isabelle P. Rosa <ibell.pereira@gmail.com>
2019-04-06 03:09:26 -0300

    main

--- src/main.c
+++ src/main.c
@@ -1,22 +1,118 @@
 #include "main.h"
 
 int main(void) {
+	const int QTD_ITEMS = 10;
+	const int RANGE = 50;
 
-	/*TipoLista *lista = criaListaPopulada(20);
-	imprimeLista(lista);
-	printf("\n");*/
+	 // |
+	 // | EXERCICIO 09
+	 // |
 
-	TipoPilha *pilha = criaPilhaPopulada(20);
-	imprimePilha(pilha);
-	printf("\n");
-	imprimePilhaPorInsercao(pilha);
-	printf("\n");
-	imprimePilha(pilha);
+	 printf("==== EXERCÍCIO 9 ====\n");
+	 TipoLista* lista = criaListaPopulada(QTD_ITEMS);
+	 printf("Lista:\n");
+	 imprimeLista(lista);
+	 printf("\nSegunda Lista: \n");
+	 imprimeLista(divideLista(lista, 0));
 
-/*	TipoFila *fila = criaFilaPopulada(20);
-	imprimeFila(fila);
-	printf("\n");
-	imprimeFila(inverteFila(fila));*/
+	 printf("\n\n\n");
 
-	return 1;
+
+	 srand(time(NULL));
+
+
+	 // |
+	 // | EXERCICIO 14
+	 // |
+
+	 printf("==== EXERCÍCIO 14 ====\n");
+	 TipoLista* lista2 = criaListaPopulada(QTD_ITEMS);
+	 printf("Lista:\n");
+	 imprimeLista(lista2);
+	 printf("\nLista com nós invertidos: \n");
+	 imprimeLista(inverteCelulas(lista2, rand()%RANGE, rand()%RANGE));
+
+	 printf("\n\n\n");
+
+	// |
+	// | EXERCICIO 15
+	// |
+		printf("==== EXERCÍCIO 15 ====\n");
+	 int i;
+
+	 printf("-- lista circular --\n\n");
+	 TipoLista* listaCircular = criaListaPopulada(0);
+
+	 for (i = 0; i < QTD_ITEMS; i++) {
+	 insereListaCircular(listaCircular, criaItem());
+	 }
+
+	 printf("Lista:\n");
+	 imprimeListaEncadeada(listaCircular);
+
+	 printf("\nLista após remoção: \n");
+	 removeListaCircularPorPos(listaCircular, rand()%RANGE);
+	 imprimeListaEncadeada(listaCircular);
+
+	 printf("\n\n");
+
+	 printf("-- lista duplamente encadeada --\n\n");
+	 TipoLista* listaDupla = criaListaPopulada(0);
+
+	 for (i = 0; i < QTD_ITEMS; i++) {
+	 insereListaDuplamenteEncadeada(listaDupla, criaItem());
+	 }
+
+	 printf("Lista:\n");
+	 imprimeListaEncadeada(listaDupla);
+	 removeListaDuplamenteEncadeadaPorPos(listaDupla, rand()%RANGE);
+	 printf("\nLista após remoção: \n");
+	 imprimeListaEncadeada(listaDupla);
+
+	 printf("\n\n");
+
+	printf("-- lista circular duplamente encadeada --\n\n");
+	 TipoLista* listaCircularDupla = criaListaPopulada(0);
+	 for (i = 0; i < QTD_ITEMS; i++) {
+	 insereListaCircularDuplamenteEncadeada(listaCircularDupla, criaItem());
+	 }
+
+	 printf("Lista:\n");
+	 imprimeListaEncadeada(listaCircularDupla);
+	 removeListaCircularDuplamenteEncadeadaPorPos(listaCircularDupla, rand()%RANGE);
+	 printf("\nLista após remoção: \n");
+	 imprimeListaEncadeada(listaCircularDupla);
+	 printf("\n\n\n");
+
+
+	 // |
+	 // | EXERCICIO 15
+	 // |
+
+
+	 printf("==== EXERCÍCIO 18 ====\n");
+	 TipoPilha* pilha = criaPilhaPopulada(QTD_ITEMS);
+	 printf("Pilha:\n");
+	 imprimePilha(pilha);
+	 printf("\nPilha ordenada por inserção: \n");
+	 imprimePilhaPorInsercao(pilha);
+
+	 printf("\n\n\n");
+
+	 // |
+	 // | EXERCICIO 22
+	 // |
+
+
+	 printf("==== EXERCÍCIO 22 ====\n");
+	 TipoFila* fila = criaFilaPopulada(QTD_ITEMS);
+	 printf("Fila:\n");
+	 imprimeFila(fila);
+	 printf("\nFila Invertida: \n");
+	 imprimeFila(inverteFila(fila));
+
+	 printf("\n\n\n");
+
+
+	 return 1;
 }
--- src/main.h
+++ src/main.h
@@ -1,19 +1,28 @@
+#ifndef MAIN_H_
+#define MAIN_H_ "main.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
-#include "node.h"
-#include "lista/lista.h"
-#include "fila/fila.h"
-#include "pilha/pilha.h"
-
+#include "tads/node.h"
+#include "tads/lista/lista.h"
+#include "exercicio-15/lista-circular/lista-circular.h"
+#include "exercicio-15/lista-duplamente-encadeada/lista-dupla.h"
+#include "exercicio-15/lista-circular-duplamente-encadeada/lista-circular-dupla.h"
+#include "tads/fila/fila.h"
+#include "tads/pilha/pilha.h"
 
 TipoLista* criaListaPopulada(int);
 TipoLista* clonaLista(TipoLista*);
 TipoFila* criaFilaPopulada(int);
 TipoPilha* criaPilhaPopulada(int);
+TipoItem criaItem();
 
 TipoLista* divideLista(TipoLista*, int);
 TipoLista* inverteCelulas(TipoLista*, int, int);
+void imprimeListaEncadeada(TipoLista*);
 TipoFila* inverteFila(TipoFila*);
 void imprimePilhaPorInsercao(TipoPilha*);
+
+#endif
